67c31194b978603ed47f9a62b383be2dee1b3bf8
Thomas Künneth <kuennetht@gmail.com>
2024-09-05 10:17:05 +0200

    Library and tools upgrades

--- RoleDemo/app/src/main/java/com/thomaskuenneth/roledemo/MainActivity.kt
+++ RoleDemo/app/src/main/java/com/thomaskuenneth/roledemo/MainActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -35,6 +37,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val manager = getSystemService(RoleManager::class.java)
         manager?.run {
             if (isRoleAvailable(RoleManagerCompat.ROLE_BROWSER)) {
@@ -67,6 +70,7 @@ class MainActivity : ComponentActivity() {
                     style = MaterialTheme.typography.displayMedium,
                     color = MaterialTheme.colorScheme.primary,
                     modifier = Modifier.align(Alignment.BottomCenter)
+                        .safeContentPadding()
                 )
             }
         }
